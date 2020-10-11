9d4114ed7dc3bee294375ce9d3285f94b0562641
mosesbenjamin <mavewreek@gmail.com>
2020-10-11 13:19:19 +0100

    Socket.io rooms

--- server/server.js
+++ server/server.js
@@ -20,13 +20,21 @@ app.use(express.static(publicPath))
 io.on('connection', (socket)=>{
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
     socket.on('join', (params, callback) => {
         if(!isRealString(params.name) || !isRealString(params.room)){
             callback('Name and room name are required')
         }
+
+        socket.join(params.room);
+        // socket.leave('Some room name');
+
+        // io.emit -> io.to('Some room name').emit
+        // socket.broadcast.emit -> socket.broadcast.to('Some room name').emit
+        // socket.emit 
+
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`))
 
         callback();
     })
